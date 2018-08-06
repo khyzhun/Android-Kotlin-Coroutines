210b8ba534b23302fd188fb0c765d7c38a59a11c
SashaKhyzhun <sasha.khyzhun@gmail.com>
2018-08-07 00:25:54 +0300

    the main problem of concurrency

--- app/src/main/java/com/sashakhyzhun/kotlincoroutines/SharedMutableStateAndConcurrency.kt
+++ app/src/main/java/com/sashakhyzhun/kotlincoroutines/SharedMutableStateAndConcurrency.kt
@@ -3,6 +3,12 @@ package com.sashakhyzhun.kotlincoroutines
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_shared_mutable_state_and_concurrency.*
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
+import kotlin.coroutines.experimental.CoroutineContext
+import kotlin.system.measureTimeMillis
 
 class SharedMutableStateAndConcurrency : AppCompatActivity() {
 
@@ -19,7 +25,14 @@ class SharedMutableStateAndConcurrency : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_mutable_state_and_concurrency)
 
-        btn_the_problem.setOnClickListener { theProblem() }
+        btn_the_problem.setOnClickListener {
+            runBlocking {
+                var counter = 0
+                theProblem(context = CommonPool) {
+                    counter++
+                }
+            }
+        }
         btn_volatiles_are_of_no_help.setOnClickListener { volatilesAreOfNoHelp() }
         btn_thread_safe_data_structures.setOnClickListener { threadSafeDataStructures() }
         btn_thread_confinement_fine_grained.setOnClickListener { threadConfinementFineGrained() }
@@ -34,7 +47,19 @@ class SharedMutableStateAndConcurrency : AppCompatActivity() {
      * (for a total of a million executions).
      * We'll also measure their completion time for further comparisons:
      */
-    private fun theProblem() {}
+    private suspend fun theProblem(context: CoroutineContext, action: suspend () -> Unit) {
+        val n = 1000
+        val k = 1000
+        val time = measureTimeMillis {
+            val jobs = List(n) {
+                launch(context) {
+                    repeat(k) { action() }
+                }
+            }
+            jobs.forEach { it.join() }
+        }
+        print("Completed ${n * k} actions in $time ms")
+    }
 
     private fun volatilesAreOfNoHelp() {}
 
